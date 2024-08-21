f5e08a2d1f4e90bfc168bbe70cd17ce1df84a82a
Lyba <faisallyba06@gmail.com>
2024-08-21 22:20:00 +0500

    React - Creating Components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Lybafunc from './component'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Lybafunc/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/component.jsx
@@ -0,0 +1,13 @@
+/* Creating and Nesting Components */
+
+import './comp.css'
+
+function Lybafunc(first) {
+    return(
+        <>
+        <h1>Hey, I am Lyba</h1>
+        <h3>This is my first React App Component</h3>
+        </>
+    )
+}
+export default Lybafunc;
