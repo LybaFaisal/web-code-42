9f8ceaf094682eff70406072d823a11548324e92
Lyba <faisallyba06@gmail.com>
2024-08-21 22:29:09 +0500

    Displaying data in the same component

--- src/component.jsx
+++ src/component.jsx
@@ -2,11 +2,16 @@
 
 import './comp.css'
 
+// How to display data in React:
+
 function Lybafunc(first) {
+    let std_data = 123456;
     return(
         <>
         <h1>Hey, I am Lyba</h1>
         <h3>This is my first React App Component</h3>
+
+        <p>Here is the students data : {std_data}</p>
         </>
     )
 }
